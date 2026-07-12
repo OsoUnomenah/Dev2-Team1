f91f429c211e1f5454df92c4fa239c1e349ee945
dburkhart751-maker <dburkhart751@gmail.com>
2026-07-11 23:31:04 -0400

    Fixed sniper FOV

--- Assets/GravewalkProtocol/Legacy(OldSystems)/Scripts/_Core/Handlers/PlayerInputHandler.cs
+++ Assets/GravewalkProtocol/Legacy(OldSystems)/Scripts/_Core/Handlers/PlayerInputHandler.cs
@@ -167,6 +167,7 @@ public class PlayerInputHandler : MonoBehaviour, IDamage
 
         standingCameraPosition = gameManager.instance.playerCamera.transform.localPosition;
         fov = gameManager.instance.playerCamera.fieldOfView;
+        normalCameraFOV = gameManager.instance.playerCamera.fieldOfView;
     }
 
     void Update()
@@ -1107,8 +1108,8 @@ public class PlayerInputHandler : MonoBehaviour, IDamage
         chargedShotCooldownTimer =
             weaponManager.ChargeCooldown;
 
-        gameManager.instance.playerCamera.fieldOfView =
-            normalCameraFOV;
+        //gameManager.instance.playerCamera.fieldOfView =
+           // normalCameraFOV;
     }
 
 
