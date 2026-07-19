c48cb8f638808d5bc570b1abf705dad06af9aa67
KWurde <kwwurdeman@student.fullsail.edu>
2026-07-18 23:55:30 -0500

    Fire Ability Rework
    Fire ability is now BurnDOT and has particle effects for the ability pickup, the weapon, and the enemy effected by BurnDOT. Also added fire boss immunity to BurnDOT.

--- Assets/GravewalkProtocol/Legacy(OldSystems)/Scripts/_Core/Handlers/PlayerInputHandler.cs
+++ Assets/GravewalkProtocol/Legacy(OldSystems)/Scripts/_Core/Handlers/PlayerInputHandler.cs
@@ -4,7 +4,6 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using static UnityEngine.GraphicsBuffer;
 
 //Steps to use
 //1. Setup bindings in Unity Editor using PlayerInputHandler ActionMap
@@ -892,7 +891,7 @@ public class PlayerInputHandler : MonoBehaviour
                 {
                     gameManager.instance.isShooting = true;
                     PlayCurrentWeaponShootSound();
-                    
+
                     // One trigger pull consumes one shell, regardless of pellet count.
                     gameManager.instance.playerWeaponManager.Ammo--;
                     _ =
@@ -945,8 +944,8 @@ public class PlayerInputHandler : MonoBehaviour
                             bulletEnd = hit.transform.position;
 
                             IDamage dmg = hit.collider.GetComponentInChildren<IDamage>();
-                            
-                            if(dmg == null)
+
+                            if (dmg == null)
                             {
 
                                 dmg = hit.collider.GetComponentInParent<IDamage>();
@@ -961,7 +960,7 @@ public class PlayerInputHandler : MonoBehaviour
                                     //Also, melee weapons call their stuff in their own methods, so you'll need to go into them and just make sure they're working
                                     //personally I'll 
                                     case AbilityStats.ability.fire:
-                                        //probably use a IFire interface that works like IDamage but makes them set fire
+                                        TryApplyWeaponFire(hit.collider);
                                         break;
                                     case AbilityStats.ability.freeze:
                                         TryApplyWeaponFreeze(hit.collider, false);
@@ -1245,6 +1244,7 @@ public class PlayerInputHandler : MonoBehaviour
                 );
             }
 
+            TryApplyWeaponFire(hit.collider);
             TryApplyWeaponFreeze(hit.collider, false);
 
             IDamage damageTarget =
@@ -1699,6 +1699,34 @@ public class PlayerInputHandler : MonoBehaviour
         return freezeStats.abilityType == AbilityStats.ability.freeze;
     }
 
+    private bool CurrentAbilityIsFire(out AbilityStats fireStats)
+    {
+        fireStats = null;
+
+        PlayerWeaponManager weaponManager = gameManager.instance.playerWeaponManager;
+
+        if (weaponManager == null || weaponManager.abilities == null)
+        {
+            return false;
+        }
+
+        if (weaponManager.abilitySlot < 0 || weaponManager.abilitySlot >= weaponManager.abilities.Count)
+        {
+            return false;
+        }
+
+        fireStats = weaponManager.abilities[weaponManager.abilitySlot];
+
+        if (fireStats == null)
+        {
+            return false;
+        }
+
+        return fireStats.abilityType == AbilityStats.ability.fire;
+    }
+
+
+
     private IFreeze FindFreezeTarget(Collider hitCollider)
     {
         IFreeze freezeTarget = hitCollider.GetComponent<IFreeze>();
@@ -1748,7 +1776,7 @@ public class PlayerInputHandler : MonoBehaviour
 
             if (gameManager.instance.playerWeaponManager.crystalHit != null && gameManager.instance.playerStatHandler.crystalBar > 9)
             {
-                effectScale = 2f;              
+                effectScale = 2f;
             }
             crystalEffect.transform.localScale = Vector3.one * effectScale;
 
@@ -1821,6 +1849,54 @@ public class PlayerInputHandler : MonoBehaviour
         {
             freezeTarget.freeze(freezeDuration);
         }
+    }
+
+    private void TryApplyWeaponFire(Collider hitCollider)
+    {
+        if (hitCollider == null)
+        {
+            return;
+        }
+
+        if (!CurrentAbilityIsFire(out AbilityStats fireStats))
+        {
+            return;
+        }
+
+        if (!RollAbilityProc())
+        {
+            return;
+        }
+
+        BurnDOT burnTarget = hitCollider.GetComponent<BurnDOT>();
+
+        if (burnTarget == null)
+        {
+            burnTarget = hitCollider.GetComponentInParent<BurnDOT>();
+        }
+
+        if (burnTarget == null)
+        {
+            burnTarget = hitCollider.GetComponentInChildren<BurnDOT>();
+        }
+
+        if (burnTarget == null)
+        {
+            Component damageComponent = hitCollider.GetComponentInParent<IDamage>() as Component;
+
+            GameObject burnHost = damageComponent != null
+                ? damageComponent.gameObject
+                : hitCollider.gameObject;
+
+            burnTarget = burnHost.GetComponent<BurnDOT>();
+
+            if (burnTarget == null)
+            {
+                burnTarget = burnHost.AddComponent<BurnDOT>();
+            }
+        }
+
+        burnTarget.ApplyBurn(3, 1f, 1, 5, fireStats.loopedEffect);
     }
 
     public bool TryShatterFrozenTarget(Collider hitCollider)
