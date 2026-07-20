b8c15fc21a09619f35ce38a02e07269a880f6611
dburkhart751-maker <dburkhart751@gmail.com>
2026-07-19 23:50:58 -0400

    Fix bug

--- Assets/GravewalkProtocol/Legacy(OldSystems)/Scripts/Bosses/BossTrigger.cs
+++ Assets/GravewalkProtocol/Legacy(OldSystems)/Scripts/Bosses/BossTrigger.cs
@@ -8,7 +8,7 @@ public class BossTrigger : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        bossTrig = Boss.GetComponent<IBossTrigger>();
+        bossTrig = Boss.GetComponentInChildren<IBossTrigger>();
     }
 
     // Update is called once per frame
